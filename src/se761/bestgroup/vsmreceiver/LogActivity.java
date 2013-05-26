7aeddda80c0312b71b9834ea239b176bc5627c4e
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-05-26 14:16:52 +1200

    Added on menu item selected to the log

--- src/se761/bestgroup/vsmreceiver/LogActivity.java
+++ src/se761/bestgroup/vsmreceiver/LogActivity.java
@@ -21,6 +21,7 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -47,6 +48,14 @@ public class LogActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.log, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		if(item.getItemId() == R.id.action_departments){
+			startActivity(new Intent(this, DepartmentsActivity.class));
+		}
 		return true;
 	}
 
